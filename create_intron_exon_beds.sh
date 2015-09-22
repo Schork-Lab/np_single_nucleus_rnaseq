a5cc19c8cbe1bd1909f4a6f688dc4873481e62fc
Kunal Bhutani <kunalbhutani@gmail.com>
2015-09-22 13:46:20 -0400

    Change filename handling to allow spaces

--- create_intron_exon_beds.sh
+++ create_intron_exon_beds.sh
@@ -6,23 +6,23 @@ EXON_BED=$2
 INTRON_BED=$3
 
 #Create Exon Bed
-cat $GTF_FILE | awk 'BEGIN{FS="\t";OFS="\t";} $3=="exon" {print $1,$4,$5,$9}' > $EXON_BED
+cat "${GTF_FILE}" | awk 'BEGIN{FS="\t";OFS="\t";} $3=="exon" {print $1,$4,$5,$9}' > "${EXON_BED}"
 
-sortBed -i $EXON_BED > temp.bed
+sortBed -i "${EXON_BED}" > temp.bed
 
-mv -f temp.bed $EXON_BED
+mv -f temp.bed "${EXON_BED}"
 
 #Merge Exons Together
 
-mergeBed -i $EXON_BED -c 1,4 -o count,collapse > tmp_merged_exon.bed
+mergeBed -i "${EXON_BED}" -c 1,4 -o count,collapse > tmp_merged_exon.bed
 
 
 #Create Intron Bed
-cat $GTF_FILE | awk 'BEGIN{FS="\t";OFS="\t";} $3=="gene" {print $1,$4,$5,$9}' > tmp.bed
+cat "${GTF_FILE}" | awk 'BEGIN{FS="\t";OFS="\t";} $3=="gene" {print $1,$4,$5,$9}' > tmp.bed
 
 sortBed -i tmp.bed > tmp_gene.bed
 
-subtractBed -a tmp_gene.bed -b tmp_merged_exon.bed > $INTRON_BED
+subtractBed -a tmp_gene.bed -b tmp_merged_exon.bed > "${INTRON_BED}"
 rm tmp_merged_exon.bed
 rm tmp.bed
 rm tmp_gene.bed
