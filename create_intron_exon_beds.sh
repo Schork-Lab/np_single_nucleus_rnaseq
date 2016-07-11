f0f78cfabcad1e72c48cd2b611b895cf8f4f7bf1
Kunal Bhutani <kunalbhutani@gmail.com>
2016-07-11 12:16:15 -0400

    Update create_intron_exon_beds
    Bug fix for creating exon file

--- create_intron_exon_beds.sh
+++ create_intron_exon_beds.sh
@@ -8,21 +8,20 @@ INTRON_BED=$3
 #Create Exon Bed
 cat "${GTF_FILE}" | awk 'BEGIN{FS="\t";OFS="\t";} $3=="exon" {print $1,$4,$5,$9}' > "${EXON_BED}"
 
-sortBed -i "${EXON_BED}" > temp.bed
+sortBed -i "${EXON_BED}" > tmp.bed
 
-mv -f temp.bed "${EXON_BED}"
+mv -f tmp.bed "${EXON_BED}"
 
 #Merge Exons Together
 
 mergeBed -i "${EXON_BED}" -c 1,4 -o count,collapse > tmp_merged_exon.bed
-
+mv -f tmp_merged_exon.bed "${EXON_BED}"
 
 #Create Intron Bed
 cat "${GTF_FILE}" | awk 'BEGIN{FS="\t";OFS="\t";} $3=="gene" {print $1,$4,$5,$9}' > tmp.bed
 
 sortBed -i tmp.bed > tmp_gene.bed
 
-subtractBed -a tmp_gene.bed -b tmp_merged_exon.bed > "${INTRON_BED}"
-rm tmp_merged_exon.bed
+subtractBed -a tmp_gene.bed -b "${EXON_BED}" > "${INTRON_BED}"
 rm tmp.bed
 rm tmp_gene.bed
